6582c1e3da545a03bf9346fa3daa7794a6cd8917
Chris Henry <henry.christopher@gmail.com>
2014-10-26 15:39:41 -0400

    My aliases file

---
+++ aliases/available/mine.aliases.bash
@@ -0,0 +1,16 @@
+#
+# Aliases
+alias ll='ls -al'
+alias la='ls -al'
+alias s='ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l chris -i ~/.ssh/id_rsa_ssh'
+
+#
+# Git aliases
+alias gb='git branch'
+alias gba='git branch -a'
+alias gcav='git commit -av'
+alias gca='git commit -a'
+alias gs='git status'
+
+alias bx='bundle exec '
+alias bek='bundle exec kitchen '
