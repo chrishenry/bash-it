c69232b23d65567a77c1f607e8cce9a9f9a2dbb7
Chris Henry <henry.christopher@gmail.com>
2015-03-21 18:23:33 -0400

    new: pretty json alias & alias to git change to master and pull with a prune.

--- aliases/available/mine.aliases.bash
+++ aliases/available/mine.aliases.bash
@@ -11,7 +11,14 @@ alias gba='git branch -a'
 alias gcav='git commit -av'
 alias gca='git commit -a'
 alias gs='git status'
+alias gcpp='gco master && gpp'
 
+#
+# Bundler aliases
 alias bx='bundle exec '
 alias bek='bundle exec kitchen '
 alias biv='bundle install --path .vendor'
+
+#
+# Prettify JSON
+alias pjson='python -m json.tool'
