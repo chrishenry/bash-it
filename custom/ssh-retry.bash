4984476e420181bb8d37e1dab9496a05da0a2b83
Chris Henry <henry.christopher@gmail.com>
2016-08-11 15:48:21 -0400

    new: Enable ssh-agent forwarding in ssh command

--- custom/ssh-retry.bash
+++ custom/ssh-retry.bash
@@ -5,7 +5,7 @@ sretry ()
   n=0
   until [ $n -ge 10 ]
   do
-    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l chris $1 && break  # substitute your command here
+    ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l chris -A $1 && break  # substitute your command here
     n=$[$n+1]
     sleep 15
   done
