03952b5c1744badbef63fcf751b0bc0a87262b08
Chris Henry <henry.christopher@gmail.com>
2017-05-03 15:05:49 -0400

    new: dco-restart and dco-rebuild

--- custom/dco.bash
+++ custom/dco.bash
@@ -4,9 +4,31 @@ dco-shell ()
 {
   if [ -z "$1" ]
   then
-    machine="web"
+    service="web"
   else
-    machine="$1"
+    service="$1"
   fi
-  docker-compose exec $machine /bin/bash
+  docker-compose exec $service /bin/bash
+}
+
+dco-rebuild ()
+{
+  if [ -z "$1" ]
+  then
+    service="web"
+  else
+    service="$1"
+  fi
+  docker-compose stop $service && docker-compose rm -f $service && docker-compose build $service && dco-up
+}
+
+dco-restart ()
+{
+  if [ -z "$1" ]
+  then
+    service="web"
+  else
+    service="$1"
+  fi
+  docker-compose restart $service && docker-compose logs -f $service
 }
